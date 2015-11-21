7a1c09895191a6772883caa2e35cd4a0d16509f8
Renaud Pawlak <renaud.pawlak@gmail.com>
2015-11-21 11:25:40 +0100

    reorganized examples

---
+++ src/main/java/org/jsweet/examples/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This is a root package. It means that this package and all its parents are
+ * ignored in the final program structure. Also, if the transpiler's
+ * noRootDirectory option is turned on, this directory and all its parents are
+ * ignored in the output file hierarchy. Root packages parents cannot contain
+ * classes.
+ */
+@jsweet.lang.Root
+package org.jsweet.examples;
